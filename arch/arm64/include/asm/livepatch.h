89f481d9eeb287eef58fd2e80469b02cdde7039f
Joe Lawrence <joe.lawrence@redhat.com>
2022-02-11 15:07:56 -0500

    (devel) Simplify CONFIG_LIVEPATCH and arches for easier unit testing
    Remove CONFIG_ dependencies and add a few function stubs to make it
    easier to build klp-converted modules for unit testing.  This only makes
    it possible to /build/ the livepatch modules, not actually load them.
    
    Signed-off-by: Joe Lawrence <joe.lawrence@redhat.com>

---
+++ arch/arm64/include/asm/livepatch.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef _ASM_X86_LIVEPATCH_H
+#define _ASM_X86_LIVEPATCH_H
+
+#include <asm/setup.h>
+#include <linux/ftrace.h>
+
+static inline void klp_arch_set_pc(struct ftrace_regs *fregs, unsigned long ip)
+{
+	ftrace_instruction_pointer_set(fregs, ip);
+}
+
+#endif /* _ASM_X86_LIVEPATCH_H */
